f4ff6b39e30d7019454f517aea86c36378d5a290
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2015-02-25 01:54:41 +0000

    Minify js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var reactify = require('reactify');
 var htmlMinifier = require('gulp-html-minifier');
+var uglify = require('gulp-uglify');
 
 gulp.task('browserify', function () {
   return browserify('./source/js/app.js')
         .transform(reactify)
         .bundle()
         .pipe(source('shopping-list.js'))
+        .pipe(buffer())
+        .pipe(uglify())
         .pipe(gulp.dest('./build/js/'));
 });
 
