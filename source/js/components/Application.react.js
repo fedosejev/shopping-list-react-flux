4297b4dc13676be2e468de9cebf58a728a5e5606
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2015-03-15 10:53:35 +0000

    Rename a method to better explain it's purpose

--- source/js/components/Application.react.js
+++ source/js/components/Application.react.js
@@ -2,14 +2,14 @@ var React = require('react');
 var ShoppingList = require('./ShoppingList.react');
 
 var Application = React.createClass({
-  getShoppingListItems: function () {
+  getInitialShoppingListItems: function () {
     return {};
   },
 
   render: function () {
     return (
       <div className="container">
-        <ShoppingList items={this.getShoppingListItems()} />
+        <ShoppingList items={this.getInitialShoppingListItems()} />
       </div>
     );
   }
